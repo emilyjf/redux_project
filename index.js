775851bb83611984d5aa3e199abde9737c86eda1
emilyjf <flanagan.emily.j@gmail.com>
2019-03-13 10:46:29 -0500

    Abstracted actions into action creators

--- index.js
+++ index.js
@@ -37,7 +37,41 @@ const TOGGLE_TODO = 'TOGGLE_TODO'
 const ADD_GOAL = 'ADD_GOAL'
 const REMOVE_GOAL = 'REMOVE_GOAL'
 
+//action creators below 
+function addTodoAction (todo) {
+    return {
+        type: ADD_TODO,
+        todo,
+    }
+}
 
+function removeTodoAction (id) {
+    return {
+        type: REMOVE_TODO,
+        id,
+    }
+}
+
+function toggleTodoAction (id) {
+    return {
+        type: TOGGLE_TODO,
+        id,
+    }
+}
+
+function addGoalAction (goal) {
+    return {
+        type: ADD_GOAL,
+        goal,
+    }
+}
+
+function removeGoalAction (id) {
+    return {
+        type: REMOVE_GOAL,
+        id,
+    }
+}
 
 //this is a reducer function
 function todos(state = [], action){
@@ -82,37 +116,20 @@ store.subscribe(() => {
     console.log('The new state is: ', store.getState())
 })
 
-store.dispatch({
-    type: ADD_TODO,
-    todo: {
+store.dispatch(addTodoAction({
       id: 0,
       name: 'Learn Redux',
       complete: false
-    }
-})
+    }))
 
-store.dispatch({
-    type: REMOVE_TODO,
-    todo: {
-      id: 1
-    }
-})
+store.dispatch(removeTodoAction(1))
 
-store.dispatch({
-    type: TOGGLE_TODO,
-    id: 0
-})
+store.dispatch(toggleTodoAction(0))
 
-store.dispatch({
-    type: ADD_GOAL,
-    goal: {
+store.dispatch(addGoalAction({
       id: 0,
       name: 'Learn Redux'
-    }
-})
+}))
 
-store.dispatch({
-    type: REMOVE_GOAL,
-    id: 0,
-})
+store.dispatch(removeGoalAction(0))
     
