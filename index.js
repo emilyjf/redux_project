6486393b4eef647a878be50389611e70cc926eb6
emilyjf <flanagan.emily.j@gmail.com>
2019-03-13 10:29:24 -0500

    Refactored actionTypes to be vars

--- index.js
+++ index.js
@@ -26,18 +26,27 @@ function createStore(reducer){
     return {
         getState,
         subscribe, 
-        dispatch
+        dispatch,
     }
 }
+
+//app code
+const ADD_TODO = 'ADD_TODO'
+const REMOVE_TODO = 'REMOVE_TODO'
+const TOGGLE_TODO = 'TOGGLE_TODO'
+const ADD_GOAL = 'ADD_GOAL'
+const REMOVE_GOAL = 'REMOVE_GOAL'
+
+
 
 //this is a reducer function
 function todos(state = [], action){
     switch(action.type){
-        case 'ADD_TODO' :
+        case ADD_TODO :
           return state.concat([action.todo])
-        case 'REMOVE_TODO' :
+        case REMOVE_TODO :
             return state.filter((todo) => todo.id !== action.id)
-        case 'TOGGLE_TODO' :
+        case TOGGLE_TODO :
             state.map((todo) => todo.id !== action.id ? todo :
             Object.assign({}, todo, { complete: !todo.complete }))
         default:
@@ -49,9 +58,9 @@ function todos(state = [], action){
 //another reducer function
 function goals (state = [], action){
     switch(action.type) {
-        case 'ADD_GOAL' :
+        case ADD_GOAL :
             return state.concat([action.goal])
-        case 'REMOVE_GOAL' :
+        case REMOVE_GOAL :
             return state.filter((goal) => goal.id !== action.id)
         default:
             return state
@@ -74,11 +83,36 @@ store.subscribe(() => {
 })
 
 store.dispatch({
-    type: 'ADD_TODO',
+    type: ADD_TODO,
     todo: {
       id: 0,
       name: 'Learn Redux',
       complete: false
     }
+})
+
+store.dispatch({
+    type: REMOVE_TODO,
+    todo: {
+      id: 1
+    }
+})
+
+store.dispatch({
+    type: TOGGLE_TODO,
+    id: 0
+})
+
+store.dispatch({
+    type: ADD_GOAL,
+    goal: {
+      id: 0,
+      name: 'Learn Redux'
+    }
+})
+
+store.dispatch({
+    type: REMOVE_GOAL,
+    id: 0,
 })
     
